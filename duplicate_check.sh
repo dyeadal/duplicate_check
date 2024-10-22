525ee672a3b788f4192a858c3db3df7f0a9a87cd
Dyeadal <dyeadal@proton.me>
2024-10-21 22:01:53 -0500

    Create duplicate_check.sh

---
+++ duplicate_check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get directory from argument
+
+# Verify valid directory
+
+# Create array to store hashes, file paths, and file name
+
+# Function to generate hashes
+
+# Run function
+
+# Check array for duplicate hashes
