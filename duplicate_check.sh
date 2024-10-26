3ee277e629eb3425bbf3ec12a3a4578652e52d54
Dyeadal <dyeadal@proton.me>
2024-10-25 23:53:30 -0500

    Update duplicate_check.sh

--- duplicate_check.sh
+++ duplicate_check.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+# Created by dyeadal
 
-# Get directory from argument
+# Check if a directory was provided as an argument, provide usage example if missing argument
+if [ $# -ne 1 ]; then 
+	echo "No argument/directory provided"
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
 
-# Verify valid directory
+# Get the directory from the argument, store directory name and full pwd
+DIR="$1"
+PWDDIR="`pwd $DIR`"
+echo "Directory to scan: $PWDDIR"
+# Prompt user to verify directory or quit
+read -p "Press any key to continue or [CTRL + C] to exit"
 
 # Create array to store hashes, file paths, and file name
 
